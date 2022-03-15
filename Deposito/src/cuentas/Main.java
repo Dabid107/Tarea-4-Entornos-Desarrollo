7d1e2b641e61adb09f2a11cee33462157cf3ef53
David <davidcasgar@gmail.com>
2022-03-15 16:48:50 +0100

    Se ha cambiado el nombre de la variable saldoActual por solo saldo para realizar un commit

--- Deposito/src/cuentas/Main.java
+++ Deposito/src/cuentas/Main.java
@@ -12,11 +12,11 @@ public class Main {
 
     public static void main(String[] args) {
         CCuenta cuenta1;
-        double saldoActual;
+        double saldo;
 
         cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
-        saldoActual = cuenta1.estado();
-        System.out.println("El saldo actual es"+ saldoActual );
+        saldo = cuenta1.estado();
+        System.out.println("El saldo actual es"+ saldo );
 
         operativa_cuenta(cuenta1, 0);
     }
